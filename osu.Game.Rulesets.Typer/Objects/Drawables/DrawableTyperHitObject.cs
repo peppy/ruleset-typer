6e56a0663a965d862a1749ed7e9817edd477b48d
Dean Herbert <pe@ppy.sh>
2022-09-16 02:25:41 +0900

    Basic gameplay

--- osu.Game.Rulesets.Typer/Objects/Drawables/DrawableTyperHitObject.cs
+++ osu.Game.Rulesets.Typer/Objects/Drawables/DrawableTyperHitObject.cs
@@ -1,46 +1,97 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Input.Events;
+using osu.Framework.Utils;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Scoring;
 using osuTK;
 using osuTK.Graphics;
+using osuTK.Input;
 
 namespace osu.Game.Rulesets.Typer.Objects.Drawables
 {
     public class DrawableTyperHitObject : DrawableHitObject<TyperHitObject>
     {
+        private readonly char keyToHit;
+
         public DrawableTyperHitObject(TyperHitObject hitObject)
             : base(hitObject)
         {
-            Size = new Vector2(40);
-            Origin = Anchor.Centre;
+            Size = new Vector2(80);
 
-            // todo: add visuals.
+            Origin = Anchor.CentreLeft;
+            Anchor = Anchor.CentreLeft;
+
+            Masking = true;
+            CornerRadius = 15;
+            CornerExponent = 2.5f;
+
+            keyToHit = (char)RNG.Next('a', 'z' + 1);
+
+            AddRangeInternal(new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Color4.Gray,
+                },
+                new OsuSpriteText
+                {
+                    Font = OsuFont.Default.With(size: 52, weight: FontWeight.Bold),
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Text = keyToHit.ToString().ToUpper(),
+                }
+            });
         }
+
+        private bool correctKey;
 
         protected override void CheckForResult(bool userTriggered, double timeOffset)
         {
-            if (timeOffset >= 0)
-                // todo: implement judgement logic
-                ApplyResult(r => r.Type = HitResult.Perfect);
+            if (userTriggered)
+            {
+                if (!correctKey || Math.Abs(timeOffset) >= 100)
+                    ApplyResult(r => r.Type = HitResult.Miss);
+                else
+                    ApplyResult(r => r.Type = HitResult.Perfect);
+            }
+            else if (timeOffset >= 100)
+                ApplyResult(r => r.Type = HitResult.Miss);
+        }
+
+        protected override bool OnKeyDown(KeyDownEvent e)
+        {
+            if (!Result.HasResult)
+            {
+                correctKey = (e.Key - Key.A == keyToHit - 'a');
+                UpdateResult(true);
+                return true;
+            }
+
+            return base.OnKeyDown(e);
         }
 
         protected override void UpdateHitStateTransforms(ArmedState state)
         {
-            const double duration = 1000;
+            const double duration = 800;
 
             switch (state)
             {
                 case ArmedState.Hit:
+                    this.ScaleTo(1.8f, duration, Easing.OutQuint);
                     this.FadeOut(duration, Easing.OutQuint).Expire();
                     break;
 
                 case ArmedState.Miss:
-
-                    this.FadeColour(Color4.Red, duration);
-                    this.FadeOut(duration, Easing.InQuint).Expire();
+                    this.FadeColour(Color4.Red, 100);
+                    this.FadeOut(500, Easing.InQuint).Expire();
                     break;
             }
         }
--- osu.Game.Rulesets.Typer/UI/TyperPlayfield.cs
+++ osu.Game.Rulesets.Typer/UI/TyperPlayfield.cs
@@ -3,7 +3,12 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
 using osu.Game.Rulesets.UI.Scrolling;
+using osuTK;
+using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Typer.UI
 {
@@ -13,9 +18,46 @@ namespace osu.Game.Rulesets.Typer.UI
         [BackgroundDependencyLoader]
         private void load()
         {
+            RelativeSizeAxes = Axes.X;
+            Height = 80;
+
+            Anchor = Anchor.CentreLeft;
+            Origin = Anchor.CentreLeft;
+
+            Padding = new MarginPadding { Left = 100 };
+
             AddRangeInternal(new Drawable[]
             {
+                new HitBox
+                {
+                    Anchor = Anchor.CentreLeft,
+                    Origin = Anchor.CentreLeft,
+                },
                 HitObjectContainer,
+            });
+        }
+    }
+
+    internal class HitBox : CompositeDrawable
+    {
+        public HitBox()
+        {
+            Masking = true;
+            CornerRadius = 15;
+            CornerExponent = 2.5f;
+
+            BorderThickness = 4;
+            BorderColour = Color4.White;
+
+            Size = new Vector2(80);
+
+            AddRangeInternal(new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = OsuColour.Gray(0.1f),
+                },
             });
         }
     }
